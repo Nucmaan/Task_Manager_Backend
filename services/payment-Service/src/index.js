be8a13960d524c58ed65aa0214795573d1e7db72
MOHAMED ABDIHAKIN MUHUMED <48615938+Nucmaan@users.noreply.github.com>
2025-03-31 15:51:27 +0300

    one

--- services/payment-Service/src/index.js
+++ services/payment-Service/src/index.js
@@ -1,21 +1,21 @@
 require("dotenv").config();
+const sequelize = require("./database/index.js");  
 const app = require("./app.js");
-const sequelize = require("./config/index.js");
 
 const PORT = process.env.PORT;
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Connection has been established successfully.");
-    app.listen(PORT, () => {
+ sequelize.authenticate()
+  .then(() => {
+    console.log("Database connection successful!");
+
+     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
-  } catch (error) {
+  })
+  .catch((error) => {
     console.error("Unable to connect to the database:", error);
-  }
-})();
+  });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
